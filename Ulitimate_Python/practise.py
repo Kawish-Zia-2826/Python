04a138df1a6fd80063e52ce1549fbb65bb4dc52a
Kawish Zia <kawishzia2826@gmail.com>
2025-03-13 22:39:19 +0500

    just update

--- Ulitimate_Python/practise.py
+++ Ulitimate_Python/practise.py
@@ -1,5 +1,13 @@
-string = "15"
-number = 7
-string_number  = int(string)
-total  = number + string_number
-print(total)
+# string = "15"
+# number = 7
+# string_number  = int(string)
+# total  = number + string_number
+# print(total)
+# text  = "this is random text"
+# for character in text:
+#   print(character)
+
+
+fruits = "Harry"
+# print(fruits[-4:-2])
+print(len(fruits))
