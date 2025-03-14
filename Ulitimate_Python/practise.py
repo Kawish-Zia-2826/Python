b1d8b391ea6d85df36b8b0f7337d9a34a41478d4
Kawish Zia <kawishzia2826@gmail.com>
2025-03-14 17:34:02 +0500

    practise

--- Ulitimate_Python/practise.py
+++ Ulitimate_Python/practise.py
@@ -8,6 +8,79 @@
 #   print(character)
 
 
-fruits = "Harry"
+fruits = "  H adsfDSF fds  rry  HHHHHH"
 # print(fruits[-4:-2])
-print(len(fruits))
+text = "dsf f"
+
+# print(fruits.upper())
+# print(fruits.lower())
+# print(fruits.lower())
+# print(fruits.strip())
+# print(fruits.rstrip("H"))
+# print(fruits.replace("H"))
+# print(fruits.split("H"))
+# print(fruits.capitalize())
+# print(len(fruits))
+# print(fruits.center(50,"."))
+# print(fruits.count("H"))
+# print(fruits.endswith("H",1,3))
+# print(fruits.endswith("H",1,3))
+# print(text.isalpha())
+# import time
+
+
+# print( time.strftime("%H:%M:%S"))
+
+
+
+# import time
+
+# if int(time.strftime("%H")) >= 0 and int(time.strftime("%H")) < 12:
+#     print("Good Morning")
+
+
+# c  = 1
+# match c:
+#     case 1:
+#         print("one")
+#     case 2:
+#         print("two")
+#     case _:
+#         print("default")
+
+
+# for k in range(1,20,2):
+#     print(k)  
+
+
+# i = 0
+# while i < 10:
+#   print(i)
+#   i += 1
+# print("Hello, World!")  # prints "Hello, World!"
+
+
+
+# i = int(input("enter a num"))
+# while i == 0:
+#   print("zero")
+#   i = int(input("enter a num"))
+# print("the loop is closed" ,i)
+
+print("pyhton is working")
+# i  =5
+# while i > 0:
+#   print(i)
+#   i -= 1
+
+# for i in range(12):
+#     if i == 10:
+#         print("its the continue loop")
+#         continue
+#     print(i+1)
+i  =  1
+while True:
+  print(i)
+  i += 1
+  if(100%i == 0):
+    break
